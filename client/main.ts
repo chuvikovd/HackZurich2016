286af880fc4a5b5070ff7164855d4d2727b4f635
Dmitrijs Cuvikovs <dmitrijs.cuvikovs@aurumit.com>
2016-09-17 01:43:20 +0300

    F**king angular defeated

--- client/main.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../typings/browser.d.ts" />
-
-import {bootstrap} from 'angular2/platform/browser';
-import {AppComponent} from './app.component.js';
-
-bootstrap(AppComponent);
